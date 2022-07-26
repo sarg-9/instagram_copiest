5a2dadbcc230f974f59aebd373cdb9a10e9a0542
sarg-9 <aliraza.gilani@devsinc.com>
2022-07-27 02:51:33 +0500

    aho

--- app/config/environments/production.rb
+++ app/config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.default charset: 'utf-8'
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'https://insta-app-ali.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://instagram-clone-ali-app.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
